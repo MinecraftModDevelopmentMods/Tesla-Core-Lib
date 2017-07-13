a1d98710620719d6d671eba9ee6322672149678c
faceofcat <candreix@gmail.com>
2017-07-13 23:51:02 +0300

    marked most methods as obsolete; added some kotlin extenders;

--- src/main/kotlin/net/ndrei/teslacorelib/compatibility/ItemStackUtil.kt
+++ src/main/kotlin/net/ndrei/teslacorelib/compatibility/ItemStackUtil.kt
@@ -3,20 +3,26 @@ package net.ndrei.teslacorelib.compatibility
 import com.google.common.collect.Lists
 import net.minecraft.item.ItemStack
 import net.minecraftforge.items.IItemHandler
+import net.minecraftforge.items.ItemHandlerHelper
 
 /**
  * Created by CF on 2017-06-28.
  */
 @Suppress("unused")
 object ItemStackUtil {
+    @Deprecated("This was for the stupid 1.10 / 1.11 compatibility. Stop using it!", ReplaceWith("ItemStack::isEmpty"))
     fun isEmpty(stack: ItemStack?): Boolean = (stack == null) || stack.isEmpty || (stack.count == 0)
 
+    @Deprecated("This was for the stupid 1.10 / 1.11 compatibility. Stop using it!", ReplaceWith("ItemStack::grow"))
     fun grow(stack: ItemStack, amount: Int) = stack.grow(amount)
 
+    @Deprecated("This was for the stupid 1.10 / 1.11 compatibility. Stop using it!", ReplaceWith("ItemStack::shrink"))
     fun shrink(stack: ItemStack, amount: Int) = stack.shrink(amount)
 
+    @Deprecated("This was for the stupid 1.10 / 1.11 compatibility. Stop using it!", ReplaceWith("ItemStack::count"))
     fun getSize(stack: ItemStack): Int = if (ItemStackUtil.isEmpty(stack)) 0 else stack.count
 
+    @Deprecated("This was for the stupid 1.10 / 1.11 compatibility. Stop using it!", ReplaceWith("ItemStack::count"))
     fun setSize(stack: ItemStack, size: Int) { stack.count = size; }
 
     fun copyWithSize(stack: ItemStack, size: Int): ItemStack {
@@ -25,6 +31,9 @@ object ItemStackUtil {
         return result
     }
 
+    fun ItemStack.copyWithCount(size: Int): ItemStack =  ItemStackUtil.copyWithSize(this, size)
+
+    @Deprecated("This was for the stupid 1.10 / 1.11 compatibility. Stop using it!", ReplaceWith("ItemStack.EMPTY"))
     val emptyStack: ItemStack
         get() = ItemStack.EMPTY
 
@@ -94,6 +103,12 @@ object ItemStackUtil {
         return stack
     }
 
+    fun insertItems(dest: IItemHandler, stack: ItemStack, simulate: Boolean): ItemStack {
+        val remaining = ItemStackUtil.insertItemInExistingStacks(dest, stack, simulate)
+        return if (remaining.isEmpty) ItemStack.EMPTY
+        else ItemHandlerHelper.insertItem(dest, remaining, simulate)
+    }
+
     fun areEqualIgnoreSize(a: ItemStack, b: ItemStack) =
         if (a.isEmpty && b.isEmpty)
             true
@@ -101,6 +116,18 @@ object ItemStackUtil {
             false
         else {
             val x = ItemStackUtil.copyWithSize(a, b.count)
-            ItemStack.areItemStacksEqual(x, b)
+            ItemStack.areItemStacksEqualUsingNBTShareTag(x, b)
         }
+    fun ItemStack.equalsIgnoreSize(b: ItemStack) = ItemStackUtil.areEqualIgnoreSize(this, b)
+
+    fun areEqualIgnoreSizeAndNBT(a: ItemStack, b: ItemStack) =
+            if (a.isEmpty && b.isEmpty)
+                true
+            else if (a.isEmpty != b.isEmpty)
+                false
+            else {
+                val x = ItemStackUtil.copyWithSize(a, b.count)
+                ItemStack.areItemStacksEqual(x, b)
+            }
+    fun ItemStack.equalsIgnoreSizeAndNBT(b: ItemStack) = ItemStackUtil.areEqualIgnoreSizeAndNBT(this, b)
 }
