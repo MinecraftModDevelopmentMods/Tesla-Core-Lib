67457868363defc5a623d8d44da41cc02b6622c2
faceofcat <candreix@gmail.com>
2017-07-22 09:32:38 +0300

    fixed ghost rendering

--- src/main/kotlin/net/ndrei/teslacorelib/render/GhostedItemRenderer.kt
+++ src/main/kotlin/net/ndrei/teslacorelib/render/GhostedItemRenderer.kt
@@ -1,36 +1,42 @@
 package net.ndrei.teslacorelib.render
 
+import net.minecraft.block.state.IBlockState
 import net.minecraft.client.Minecraft
-import net.minecraft.client.renderer.GlStateManager
-import net.minecraft.client.renderer.RenderItem
+import net.minecraft.client.renderer.*
+import net.minecraft.client.renderer.block.model.BakedQuad
 import net.minecraft.client.renderer.block.model.IBakedModel
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms
+import net.minecraft.client.renderer.color.ItemColors
 import net.minecraft.client.renderer.texture.TextureManager
 import net.minecraft.client.renderer.texture.TextureMap
+import net.minecraft.client.renderer.texture.TextureUtil
 import net.minecraft.client.renderer.tileentity.TileEntityItemStackRenderer
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats
 import net.minecraft.crash.CrashReport
+import net.minecraft.entity.EntityLivingBase
 import net.minecraft.item.ItemStack
+import net.minecraft.util.EnumFacing
 import net.minecraft.util.ReportedException
+import net.minecraft.util.ResourceLocation
+import net.minecraft.world.World
 import net.minecraftforge.fml.relauncher.Side
 import net.minecraftforge.fml.relauncher.SideOnly
-import net.ndrei.teslacorelib.compatibility.findDeclaredField
-import net.ndrei.teslacorelib.compatibility.findDeclaredMethod
 
 /**
  * Created by CF on 2017-07-05.
  */
 @SideOnly(Side.CLIENT)
 class GhostedItemRenderer(private val render: RenderItem) {
-    private var currentAlpha: Float = .42f
+    var zLevel: Float = 0.0f
+    private val RES_ITEM_GLINT = ResourceLocation("textures/misc/enchanted_item_glint.png")
 
     fun renderItemInGUI(stack: ItemStack, x: Int, y: Int, alpha: Float = .42f) {
-        this.currentAlpha = alpha
         if (!stack.isEmpty) {
             val player = Minecraft.getMinecraft().player
-            this.render.zLevel += 50.0f
+            this.zLevel += 50.0f
 
             try {
-                this.renderItemModelIntoGUI(stack, x, y, this.render.getItemModelWithOverrides(stack, null, player))
+                this.renderItemModelIntoGUI(stack, x, y, this.render.getItemModelWithOverrides(stack, null, player), alpha)
             } catch (throwable: Throwable) {
                 val crashReport = CrashReport.makeCrashReport(throwable, "Rendering item")
                 val crashReportCategory = crashReport.makeCategory("Item being rendered")
@@ -41,72 +47,154 @@ class GhostedItemRenderer(private val render: RenderItem) {
                 throw ReportedException(crashReport)
             }
 
-            this.render.zLevel -= 50.0f
+            this.zLevel -= 50.0f
         }
     }
 
-    private fun renderItemModelIntoGUI(stack: ItemStack, x: Int, y: Int, bakedmodel: IBakedModel) {
-        val tmField = this.render.javaClass.findDeclaredField("textureManager") ?: return
-        tmField.isAccessible = true
-        val textureManager = tmField.get(this.render) as TextureManager
+    //#region stolen from RenderItem
 
-        val sgtMethod = this.render.javaClass.findDeclaredMethod("setupGuiTransform",
-                java.lang.Integer.TYPE, java.lang.Integer.TYPE, java.lang.Boolean.TYPE) ?: return
-        sgtMethod.isAccessible = true
+    fun getItemModelWithOverrides(stack: ItemStack, worldIn: World?, entitylivingbaseIn: EntityLivingBase?): IBakedModel {
+        val baked = this.render.itemModelMesher.getItemModel(stack)
+        return baked.overrides.handleItemState(baked, stack, worldIn, entitylivingbaseIn)
+    }
 
+    val textureManager: TextureManager
+        get() = Minecraft.getMinecraft().renderEngine
+
+    val itemColors: ItemColors
+        get() = Minecraft.getMinecraft().itemColors
+
+    private  fun renderItemModelIntoGUI(stack: ItemStack, x: Int, y: Int, bakedmodel: IBakedModel, alpha: Float) {
         GlStateManager.pushMatrix()
-        textureManager.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE)
-        textureManager.getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).setBlurMipmap(false, false)
+        this.textureManager.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE)
+        this.textureManager.getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).setBlurMipmap(false, false)
         GlStateManager.enableRescaleNormal()
 //        GlStateManager.enableAlpha()
-//        GlStateManager.alphaFunc(GL11.GL_GREATER, .8F)
+//        GlStateManager.alphaFunc(516, 0.1f)
         GlStateManager.enableBlend()
         GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA)
-        GlStateManager.color(1.0f, 1.0f, 1.0f, .42f)
-        // this.render.setupGuiTransform(x, y, bakedmodel.isGui3d)
-        sgtMethod.invoke(this.render, x, y, bakedmodel.isGui3d)
-        val itemBakedModel = net.minecraftforge.client.ForgeHooksClient.handleCameraTransforms(bakedmodel, ItemCameraTransforms.TransformType.GUI, false)
-        // this.render.renderItem(stack, bakedmodel)
-        this.renderItem(stack, itemBakedModel)
+        GlStateManager.color(1.0f, 1.0f, 1.0f, alpha)
+        this.setupGuiTransform(x, y, bakedmodel.isGui3d)
+        val finalModel = net.minecraftforge.client.ForgeHooksClient.handleCameraTransforms(bakedmodel, ItemCameraTransforms.TransformType.GUI, false)
+        this.renderItem(stack, finalModel, alpha)
         GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f)
-        GlStateManager.disableAlpha()
+//        GlStateManager.disableAlpha()
         GlStateManager.disableRescaleNormal()
         GlStateManager.disableLighting()
         GlStateManager.popMatrix()
-        textureManager.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE)
-        textureManager.getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).restoreLastBlurMipmap()
+        this.textureManager.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE)
+        this.textureManager.getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).restoreLastBlurMipmap()
     }
 
-    private fun renderItem(stack: ItemStack, model: IBakedModel) {
-        val rmMethod = this.render.javaClass.findDeclaredMethod("renderModel",
-                IBakedModel::class.java, java.lang.Integer.TYPE, ItemStack::class.java) ?: return
-        rmMethod.isAccessible = true
-
-        val reMethod = this.render.javaClass.findDeclaredMethod("renderEffect",
-                IBakedModel::class.java) ?: return
-        reMethod.isAccessible = true
-
+    fun renderItem(stack: ItemStack, model: IBakedModel, alpha: Float) {
         if (!stack.isEmpty) {
             GlStateManager.pushMatrix()
             GlStateManager.translate(-0.5f, -0.5f, -0.5f)
 
             if (model.isBuiltInRenderer) {
-                // GlStateManager.color(1.0f, 1.0f, 1.0f, .42f)
+//                GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f)
                 GlStateManager.enableRescaleNormal()
                 TileEntityItemStackRenderer.instance.renderByItem(stack)
             } else {
-                // this.render.renderModel(model, stack)
-                rmMethod.invoke(this.render, model, (this.currentAlpha * 255f).toInt().shl(24) + 0xFFFFFF, stack)
+                this.renderModel(model, stack, alpha)
 
                 if (stack.hasEffect()) {
-                    // this.render.renderEffect(model)
-                    reMethod.invoke(this.render, model)
+                    this.renderEffect(model)
                 }
             }
 
             GlStateManager.popMatrix()
         }
     }
+
+    private fun renderEffect(model: IBakedModel) {
+        GlStateManager.depthMask(false)
+        GlStateManager.depthFunc(514)
+        GlStateManager.disableLighting()
+        GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_COLOR, GlStateManager.DestFactor.ONE)
+        this.textureManager.bindTexture(RES_ITEM_GLINT)
+        GlStateManager.matrixMode(5890)
+        GlStateManager.pushMatrix()
+        GlStateManager.scale(8.0f, 8.0f, 8.0f)
+        val f = (Minecraft.getSystemTime() % 3000L).toFloat() / 3000.0f / 8.0f
+        GlStateManager.translate(f, 0.0f, 0.0f)
+        GlStateManager.rotate(-50.0f, 0.0f, 0.0f, 1.0f)
+        this.renderModel(model, -8372020, 1.0f)
+        GlStateManager.popMatrix()
+        GlStateManager.pushMatrix()
+        GlStateManager.scale(8.0f, 8.0f, 8.0f)
+        val f1 = (Minecraft.getSystemTime() % 4873L).toFloat() / 4873.0f / 8.0f
+        GlStateManager.translate(-f1, 0.0f, 0.0f)
+        GlStateManager.rotate(10.0f, 0.0f, 0.0f, 1.0f)
+        this.renderModel(model, -8372020, 1.0f)
+        GlStateManager.popMatrix()
+        GlStateManager.matrixMode(5888)
+        GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA)
+        GlStateManager.enableLighting()
+        GlStateManager.depthFunc(515)
+        GlStateManager.depthMask(true)
+        this.textureManager.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE)
+    }
+
+    private fun renderModel(model: IBakedModel, stack: ItemStack, alpha: Float) {
+        this.renderModel(model, (alpha * 255f).toInt().shl(24) + 0xFFFFFF, stack, alpha)
+    }
+
+    private fun renderModel(model: IBakedModel, color: Int, alpha: Float) {
+        this.renderModel(model, color, ItemStack.EMPTY, alpha)
+    }
+
+    private fun renderModel(model: IBakedModel, color: Int, stack: ItemStack, alpha: Float) {
+        val tessellator = Tessellator.getInstance()
+        val bufferbuilder = tessellator.buffer
+        bufferbuilder.begin(7, DefaultVertexFormats.ITEM)
+
+        for (enumfacing in EnumFacing.values()) {
+            this.renderQuads(bufferbuilder, model.getQuads(null as IBlockState?, enumfacing, 0L), color, stack, alpha)
+        }
+
+        this.renderQuads(bufferbuilder, model.getQuads(null as IBlockState?, null as EnumFacing?, 0L), color, stack, alpha)
+        tessellator.draw()
+    }
+
+    private fun renderQuads(renderer: BufferBuilder, quads: List<BakedQuad>, color: Int, stack: ItemStack, alpha: Float) {
+        val flag = /*color == -1 &&*/ !stack.isEmpty
+        var i = 0
+
+        val j = quads.size
+        while (i < j) {
+            val bakedquad = quads[i]
+            var k = color
+
+            if (flag && bakedquad.hasTintIndex()) {
+                k = this.itemColors.getColorFromItemstack(stack, bakedquad.tintIndex)
+
+                if (EntityRenderer.anaglyphEnable) {
+                    k = TextureUtil.anaglyphColor(k)
+                }
+
+                k = (alpha * 255f).toInt().shl(24) + (k or -16777216)
+            }
+
+            net.minecraftforge.client.model.pipeline.LightUtil.renderQuadColor(renderer, bakedquad, k)
+            ++i
+        }
+    }
+
+    private fun setupGuiTransform(xPosition: Int, yPosition: Int, isGui3d: Boolean) {
+        GlStateManager.translate(xPosition.toFloat(), yPosition.toFloat(), 100.0f + this.zLevel)
+        GlStateManager.translate(8.0f, 8.0f, 0.0f)
+        GlStateManager.scale(1.0f, -1.0f, 1.0f)
+        GlStateManager.scale(16.0f, 16.0f, 16.0f)
+
+        if (isGui3d) {
+            GlStateManager.enableLighting()
+        } else {
+            GlStateManager.disableLighting()
+        }
+    }
+
+    //#endregion
 
     companion object {
         fun renderItemInGUI(render: RenderItem, stack: ItemStack, x: Int, y: Int, alpha: Float = .42f) {
