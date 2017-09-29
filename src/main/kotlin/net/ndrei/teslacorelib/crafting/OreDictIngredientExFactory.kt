83c6c32fe6d37bd65590dec91fd380ab6b42147e
faceofcat <candreix@gmail.com>
2017-09-29 21:06:35 +0300

    improved factory to accept item names too

--- src/main/kotlin/net/ndrei/teslacorelib/crafting/OreDictIngredientExFactory.kt
+++ src/main/kotlin/net/ndrei/teslacorelib/crafting/OreDictIngredientExFactory.kt
@@ -2,9 +2,11 @@ package net.ndrei.teslacorelib.crafting
 
 import com.google.gson.JsonObject
 import com.google.gson.JsonSyntaxException
+import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.item.crafting.Ingredient
 import net.minecraft.util.JsonUtils
+import net.minecraft.util.ResourceLocation
 import net.minecraftforge.common.crafting.IIngredientFactory
 import net.minecraftforge.common.crafting.JsonContext
 import net.minecraftforge.oredict.OreDictionary
@@ -23,10 +25,31 @@ class OreDictIngredientExFactory : IIngredientFactory {
     }
 
     companion object {
-        fun getMatchingStacks(vararg oreNames: String): Array<ItemStack> =
-            oreNames
-                .mapNotNull { OreDictionary.getOres(it) }
-                .fold(mutableListOf<ItemStack>()) { list, it -> list.also { _ -> list.addAll(it) } }
-                .toTypedArray()
+        fun getMatchingStacks(vararg oreNames: String): Array<ItemStack> {
+            val ingredients = mutableListOf<ItemStack>()
+
+            oreNames.forEach {
+                var added = false
+                if (it.contains(':')) {
+                    // probably a registry name
+                    val parts = it.split(':')
+                    val meta = if (parts.size > 2) {
+                        parts[2].toIntOrNull() ?: 0
+                    } else 0
+                    val item = Item.REGISTRY.getObject(ResourceLocation(parts[0], parts[1]))
+                    if (item != null) {
+                        ingredients.add(ItemStack(item, 1, meta))
+                        added = true
+                    }
+                }
+                if (!added) {
+                    OreDictionary.getOres(it)?.forEach {
+                        ingredients.add(it)
+                    }
+                }
+            }
+
+            return ingredients.toTypedArray()
+        }
     }
 }
