90ec74b5a5924d7c6bc01b980472bd9f2c7d6ebd
faceofcat <candreix@gmail.com>
2017-10-16 23:27:51 +0300

    more bakery magic

--- src/main/kotlin/net/ndrei/teslacorelib/render/selfrendering/IRawFigure.kt
+++ src/main/kotlin/net/ndrei/teslacorelib/render/selfrendering/IRawFigure.kt
@@ -1,14 +1,10 @@
 package net.ndrei.teslacorelib.render.selfrendering
 
-import net.minecraft.client.renderer.block.model.BakedQuad
 import net.minecraft.client.renderer.texture.TextureAtlasSprite
-import net.minecraft.client.renderer.vertex.VertexFormat
-import net.minecraftforge.common.model.TRSRTransformation
-import javax.vecmath.Matrix4d
 
-interface IRawFigure {
+interface IRawFigure : IBakeable {
     fun getFaces(): List<IRawFace>
-    fun bake(quads: MutableList<BakedQuad>, format: VertexFormat, transform: TRSRTransformation, matrix: Matrix4d? = null)
+    // fun bake(quads: MutableList<BakedQuad>, format: VertexFormat, transform: TRSRTransformation, matrix: Matrix4d? = null)
 
     fun clone(sprite: TextureAtlasSprite? = null, reTexture: Boolean = false): IRawFigure
 }
