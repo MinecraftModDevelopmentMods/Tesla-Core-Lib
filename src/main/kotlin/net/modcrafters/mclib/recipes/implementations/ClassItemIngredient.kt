b939bfabd09803b12d4571d27cfa9bee27393d5e
faceofcat <candreix@gmail.com>
2017-11-16 20:50:38 +0200

    trying to fix old kotlin plugin compiling

--- src/main/kotlin/net/modcrafters/mclib/recipes/implementations/ClassItemIngredient.kt
+++ src/main/kotlin/net/modcrafters/mclib/recipes/implementations/ClassItemIngredient.kt
@@ -4,9 +4,9 @@ import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 
 class ClassItemIngredient(classNameMask: String, val quantity: Int, val meta: Int): BaseFilterItemIngredient(classNameMask) {
-    private val lazyItemStacks by lazy {
+    private val lazyItemStacks: List<ItemStack> by lazy {
         Item.REGISTRY.filter {
-            this.isMatch(it.javaClass.simpleName)
+            this.isMatch(it::class.java.simpleName)
         }.map {
             ItemStack(it, this.quantity, this.meta)
         }
